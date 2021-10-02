cd3268ad7526ddf77de814a7003e683916bd805f
hilkojj <hilkojj@outlook.com>
2021-10-03 00:05:25 +0200

    start menu stuff

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+var highestWindowZIndex = 3;
+ 
+// id: ""
+function windowdrag() 
+{
 
+}
+
+/**
+ * id: "",
+ * title: "",
+ * bodyHTML: ``,
+ * minimize: 1,
+ * maximize: 1,
+ * onClose: () => {},
+ * buttons: {}
+ */
 function makeWindow(windowOptions)
 {
     let div = document.createElement("div");
     div.innerHTML = `
 
-<div class="window" style="margin: 32px; width: 250px" ${windowOptions.id ? `id="${windowOptions.id}"` : ""}>
+<div class="window" style="width: 250px; z-index:${highestWindowZIndex++}" ${windowOptions.id ? `id="${windowOptions.id}"` : ""}
+    ondragstart="windowdrag(this)">
 <div class="title-bar">
     <div class="title-bar-text">
         ${windowOptions.title}
@@ -69,9 +86,9 @@ function BLUE_SCREEN_OF_DEATH_BABY()
     scr.style.background = "blue"
 }
 
-window.onload = () => {
-
-    const sys32FilesToDelete = 10;
+function startDeletingSys32()
+{
+    const sys32FilesToDelete = 100;
     let sys32FilesDeleted = 0;
     const maxSquaresInBar = 28;
 
@@ -82,7 +99,7 @@ window.onload = () => {
         id: "sys32-deleting-window",
         title: "Deleting C:/System32....",
         bodyHTML: `
-        <p>Deleted <span id="sys32-files-deleted"></span>/${sys32FilesToDelete}</p>
+        <p>Deleted <span id="sys32-files-deleted"></span>/${sys32FilesToDelete} files.</p>
         <input id="sys32-deleting-progress-bar" class="progress-bar" disabled="" type="text" value="&#9646;&#9646;&#9646;">
         <br><br>
         `,
@@ -141,5 +158,83 @@ window.onload = () => {
         
         document.getElementById("sys32-deleting-progress-bar").value = "▮".repeat((percentage * maxSquaresInBar) | 0 + 1);
     }, 50)
+}
+
+function showStartMenu()
+{
+    let startMenu = document.getElementById("start-menu");
+    startMenu.style.display = "block";
+
+    
+}
+
+function hideStartMenu(event)
+{
+    let startMenu = document.getElementById("start-menu");
+    
+    if (event.target == startMenu || event.target.classList.contains("program-link"))
+        startMenu.style.display = "none";
+}
+
+function notEnoughRamPopup(programName)
+{
+    let popup = makeWindow({
+        id: "not-enough-ram-window",
+        title: "Unstable system error",
+        bodyHTML: `
+        <p>Cannot open '${programName}', not enough RAM available.
+        <br>
+        Install or download more RAM.</p>
+        <br><br>
+        `,
+        // minimize: 1,
+        // maximize: 1,
+        onClose: () => {
+            popup.remove()
+        },
+        buttons: {
+            OK: event => {
+                popup.remove()
+            }
+        }
+    })
+    document.getElementById("screen").append(popup);
+}
+
+
+function openMinesweeper()
+{
+
+    notEnoughRamPopup("Minesweeper.exe")
+
+    
+}
+
+
+function openInterwebExplorer()
+{
+    let ie = makeWindow({
+        id: "interweb-explorer",
+        title: "Interweb Explorer",
+        bodyHTML: `
+        pornhub.com
+        <br><br>
+        `,
+        // minimize: 1,
+        // maximize: 1,
+        onClose: () => {
+            ie.remove()
+        }
+    })
+    let ieWindow = ie.getElementsByClassName("window")[0];
+
+    ieWindow.style.width = "calc(100% - 80px)"
+    ieWindow.style.height = "calc(100% - 80px)"
+    document.getElementById("screen").append(ie);
+}
+
+window.onload = () => {
+
+    console.log("Ludum dareee");
     
 }
